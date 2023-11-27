e7d55d1d289f3765e1d91ad423cd24ac0d7d3135
Michel Meyer <syrion@outlook.com>
2023-11-27 17:45:57 +0100

    Ajout nav isActive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="header-banner">
       <img src={logo} alt="logo Kasa" />
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/about">A propos</Link>
+      <nav className="navbar">
+        <NavLink to="/" className={(nav) => (nav.isActive ? "nav-active" : "")}>
+          Accueil
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={(nav) => (nav.isActive ? "nav-active" : "")}
+        >
+          A propos
+        </NavLink>
       </nav>
     </div>
   );
