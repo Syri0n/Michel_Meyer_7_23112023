b85b510ac167a77a33462f705bdfcc89d170b786
Michel Meyer <syrion@outlook.com>
2023-11-27 13:52:17 +0100

    mise en place routeur + pages home, about et component Error

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Error from "./components/Error";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
